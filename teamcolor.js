aa22972b7d3452a709209f0b2dd7e5bee1c8d2da
figamin <16734517+figamin@users.noreply.github.com>
2025-03-01 09:36:18 -0500

    osaka stream 3

--- teamcolor.js
+++ teamcolor.js
@@ -11,7 +11,6 @@ function setTeamList(listOfTeams){
 	});
 };
 
-$('#chatwrap').append("<span><select id='teamcolor' style='cursor:default;padding:6px 10px' class='btn btn-sm btn-default'><option value=''>Chat Icon</option></select></span>");
 var TEAMCOLOR = getOrDefault(CHANNEL.name + "_TEAMCOLOR", '');
 setTeamList(teamList_4cc);
 if (TEAMCOLOR){
